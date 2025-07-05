f06e9f8ba118cf8b75c9894e281c91147f34031e
Juan Luis Cano Rodríguez <hello@juanlu.space>
2025-07-05 14:26:43 +0200

    Add script to ingest data

---
+++ scripts/ingest-data.py
@@ -0,0 +1,32 @@
+import asyncio
+import datetime as dt
+import os
+
+from obstore.store import S3Store
+
+from monitor_madrid_parks.io import ingest_alerts, fetch_alerts
+
+
+async def main():
+    filename = f"result_{dt.datetime.now():%Y-%m-%dT%H:%M}.json"
+
+    data = await fetch_alerts()
+
+    client_options = {}
+    if os.environ.get("S3_CLIENT_ALLOW_HTTP", "false").lower() == "true":
+        client_options["allow_http"] = True
+
+    store = S3Store(
+        os.environ["S3_BUCKET"],
+        client_options=client_options,
+        endpoint=os.environ["S3_ENDPOINT"],
+        access_key_id=os.environ["S3_ACCESS_KEY_ID"],
+        secret_access_key=os.environ["S3_SECRET_ACCESS_KEY"],
+        region=os.environ["S3_REGION"],
+    )
+
+    await ingest_alerts(data, filename, store)
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
